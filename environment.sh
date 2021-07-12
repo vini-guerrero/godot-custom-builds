c9ad9b0dd3776294c31da368ae64feb8d30122e3
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-12 20:15:21 -0300

    Updated Actions

--- environment.sh
+++ environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update
-apt-get install -y --no-install-recommends sudo curl git python python-openssl wget zip unzip
-apt-get -y install build-essential pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
-python -c "import sys; print(sys.version)" && python -m pip install scons && python --version && scons --version
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends sudo curl git python python-openssl wget zip unzip
+sudo apt-get -y install build-essential pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
+sudo python -c "import sys; print(sys.version)" && sudo python -m pip install scons && sudo python --version && sudo scons --version
