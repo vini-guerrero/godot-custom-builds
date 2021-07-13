da126b6c41aa00903fb48c930460d55ee17743fa
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-12 21:19:16 -0300

    Updated Actions

--- environment.sh
+++ environment.sh
@@ -2,6 +2,6 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends sudo curl git python python3-pip python-openssl wget zip unzip mingw-w64
+sudo apt-get install -y --no-install-recommends sudo curl git python python3-pip python-openssl wget zip unzip mingw-w64 scons
 sudo apt-get -y install build-essential pkg-config libx11-dev libxcursor-dev libxinerama-dev libgl1-mesa-dev libglu-dev libasound2-dev libpulse-dev libudev-dev libxi-dev libxrandr-dev yasm
-sudo python -c "import sys; print(sys.version)" && sudo python -m /usr/bin/pip3 install scons && sudo python --version && sudo scons --version
+sudo python -c "import sys; print(sys.version)" && sudo python --version && sudo scons --version
