a7ad648d16abdc840d77566f6e09b589b15f135c
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-16 16:08:16 -0300

    Action Testing

--- build/build.sh
+++ build/build.sh
@@ -4,3 +4,6 @@ set -e
 echo "Godot Version ${GODOT_VERSION} "
 wget https://github.com/godotengine/godot/archive/${GODOT_VERSION}-stable.zip
 unzip ${GODOT_VERSION}-stable.zip
+
+scons -j2 verbose=yes warnings=all werror=yes platform=linuxbsd tools=yes tests=no target=release_debug production=yes
+scons -j2 verbose=yes warnings=all werror=yes platform=windows tools=yes tests=no target=release_debug production=yes
