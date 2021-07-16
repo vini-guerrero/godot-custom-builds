5e7c64c9ef846c9bcfae131024f60bb130b9ac9f
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-16 16:24:19 -0300

    Action Testing

--- build/build.sh
+++ build/build.sh
@@ -5,9 +5,9 @@ echo -e "\n Godot Version ${GODOT_VERSION} \n"
 wget https://github.com/godotengine/godot/archive/${GODOT_VERSION}-stable.zip
 unzip ${GODOT_VERSION}-stable.zip
 
-scons -j2 verbose=yes warnings=all werror=yes platform=linuxbsd tools=yes tests=no target=release_debug production=yes
-scons -j2 verbose=yes warnings=all werror=yes platform=windows tools=yes tests=no target=release_debug
-scons verbose=yes warnings=all werror=yes platform=osx tools=yes target=release_debug debug_symbols=no --jobs=2
+# scons -j2 verbose=yes warnings=all werror=yes platform=linuxbsd tools=yes tests=no target=release_debug production=yes
+# scons -j2 verbose=yes warnings=all werror=yes platform=windows tools=yes tests=no target=release_debug
+# scons verbose=yes warnings=all werror=yes platform=osx tools=yes target=release_debug debug_symbols=no --jobs=2
 
-echo -e "\n Godot Engine Compile Outputs \n"
+# echo -e "\n Godot Engine Compile Outputs \n"
 pwd && ls
