0b87bff8cbc27eb8c5f9e1b711339da0d0472db9
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-16 15:52:08 -0300

    Action Testing

--- environment/build.sh
+++ environment/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Received Godot Version ${GODOT_VERSION} "
+echo "Building Godot Version ${GODOT_VERSION} "
+
+wget https://github.com/godotengine/godot/archive/${GODOT_VERSION}-stable.zip
+unzip ${GODOT_VERSION}-stable.zip
+ls && pwd
