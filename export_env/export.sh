0dc1b2e0adcc912571d1d554c1a448e49fad7783
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-17 02:16:24 -0300

    Test Action

--- export_env/export.sh
+++ export_env/export.sh
@@ -3,27 +3,40 @@ set -e
 
 echo "\n\n ✔ Export Script Triggered Successfully. \n\n "
 
-echo -e "\n Build Platform ${{inputs.environment}} \n" &&
-echo -e "\n android_sdk_path -> ${ANDROID_HOME} \n adb -> ${ANDROID_HOME}/platform-tools \n" 
+# Environment Variables
+GODOT_RELEASE="${ROOT_PATH:="stable"}" &&
+ROOT_PATH="${ROOT_PATH:="/github/home"}" &&
+WORKSPACE_PATH="${WORKSPACE_PATH:="/github/workspace"}" &&
+TRES_PATH=${ROOT_PATH}/.config/godot/editor_settings-3.tres &&
+LINK_GODOT="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip" &&
+LINK_TEMPLATES="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz" &&
 
-# # Environment Variables
-# GODOT_RELEASE="${ROOT_PATH:="stable"}" &&
-# ROOT_PATH="${ROOT_PATH:="/github/home"}" &&
-# WORKSPACE_PATH="${WORKSPACE_PATH:="/github/workspace"}" &&
-# TRES_PATH=${ROOT_PATH}/.config/godot/editor_settings-3.tres &&
-# LINK_GODOT="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip" &&
-# LINK_TEMPLATES="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz" &&
+wget ${LINK_GODOT} &&
+wget ${LINK_TEMPLATES} &&
+sudo mkdir -p -v $ROOT_PATH/.cache && sudo mkdir -p -v $ROOT_PATH/.config/godot &&
+sudo mkdir -p -v $ROOT_PATH/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE} &&
 
-# wget ${LINK_GODOT} &&
-# wget ${LINK_TEMPLATES} &&
-# sudo mkdir -p -v $ROOT_PATH/.cache && sudo mkdir -p -v $ROOT_PATH/.config/godot &&
-# sudo mkdir -p -v $ROOT_PATH/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE} &&
+unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip && sudo mv Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64 /usr/local/bin/godot &&
+unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz && sudo mv templates/* $ROOT_PATH/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE} &&
 
-# unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip && sudo mv Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64 /usr/local/bin/godot &&
-# unzip Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz && sudo mv templates/* $ROOT_PATH/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE} &&
+echo "\n ✔ Godot Editor First Launch. \n " &&
+cd / && chmod +x /usr/local/bin/godot && godot -e -q &&
+echo "\n ✔ Godot Editor Launched. \n " &&
 
-# echo "\n ✔ Godot Editor First Launch. \n " &&
-# cd / && chmod +x /usr/local/bin/godot && godot -e -q &&
-# echo "\n ✔ Godot Editor Launched. \n " &&
+# Validate Editor Settings
+cat ${TRES_PATH}
+# Move To Export Path
+cd ${WORKSPACE_PATH}/${EXPORT_PATH} && ls
 
-# sudo keytool -keyalg RSA -genkeypair -alias androiddebugkey -keypass android -keystore debug.keystore -storepass android -dname "CN=Android Debug,O=Android,C=US" -validity 9999 && pwd && ls -l
+# Generate Keystore
+sudo keytool -keyalg RSA -genkeypair -alias androiddebugkey -keypass android -keystore debug.keystore -storepass android -dname "CN=Android Debug,O=Android,C=US" -validity 9999 && pwd && ls -l
+
+# Set Editor Settings For Android Export
+sed -i '/\[resource\]/a export\/android\/android_sdk_path = "/root/android-sdk"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/adb = "/root/android-sdk/platform-tools/adb"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/jarsigner = "/usr/bin/jarsigner"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/apksigner = "/usr/bin/apksigner"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/debug_keystore = "debug.keystore"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/debug_user = "androiddebugkey"' ${TRES_PATH} \
+&& sed -i '/\[resource\]/a export\/android\/debug_pass = "android"' ${TRES_PATH}
+echo "✔ Android Project Export Setup Ready"  
