30f3b2c10cf05ae9829a2a6a323b775e3e400ccd
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-17 03:14:50 -0300

    Update export.sh

--- export_env/export.sh
+++ export_env/export.sh
@@ -28,6 +28,7 @@ cd / && chmod +x /usr/local/bin/godot && godot -e -q
 echo "\n ✔ Godot Editor Launched. \n "
 
 # Validate Editor Settings
+which editor_settings-3.tres 
 cat ${TRES_PATH}
 # Move To Export Path
 cd ${WORKSPACE_PATH}/${EXPORT_PATH} && ls
