f244e91ceb07d0f234e8a1e848703c04d4bc40c7
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-19 18:38:24 -0300

    SDK Test

--- android.sh
+++ android.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 apt-get update && apt-get install -y --no-install-recommends sudo ca-certificates curl git python python-openssl unzip wget zip openjdk-8-jdk apksigner dirmngr apt-transport-https lsb-release # graphicsmagick
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt -y install nodejs 
@@ -12,7 +15,7 @@ wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_la
 unzip -q commandlinetools-linux-*.zip
 rm commandlinetools-linux-*.zip
 mv cmdline-tools latest
-mkdir -p -v /root/.android && echo "count=0" > /root/.android/repositories.cfg
-yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --licenses
-yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;21.4.7075529"
-cd /root && rm -rf /root/android-sdk-installer 
+sudo mkdir -p -v /root/.android && echo "count=0" > /root/.android/repositories.cfg
+sudo yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --licenses
+sudo yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;21.4.7075529"
+sudo cd /root && rm -rf /root/android-sdk-installer 
