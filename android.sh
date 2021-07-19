04fc97e57f01923ee638a26006f612a866b46890
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-19 18:40:53 -0300

    SDK Test

--- android.sh
+++ android.sh
@@ -12,10 +12,10 @@ rm -rf /var/lib/apt/lists/*
 sudo mkdir -p -v /root/android-sdk-installer/cmdline-tools && cd /root/android-sdk-installer/cmdline-tools
 wget "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip"
 # curl -fsSLO "https://dl.google.com/android/repository/commandlinetools-linux-6858069_latest.zip"
-unzip -q commandlinetools-linux-*.zip
-rm commandlinetools-linux-*.zip
-mv cmdline-tools latest
+sudo unzip -q commandlinetools-linux-*.zip
+sudo rm commandlinetools-linux-*.zip
+sudo mv cmdline-tools latest
 sudo mkdir -p -v /root/.android && echo "count=0" > /root/.android/repositories.cfg
 sudo yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --licenses
 sudo yes | /root/android-sdk-installer/cmdline-tools/latest/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;21.4.7075529"
-sudo cd /root && rm -rf /root/android-sdk-installer 
+cd /root && sudo rm -rf /root/android-sdk-installer 
