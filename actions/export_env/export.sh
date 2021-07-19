b04a5858f71ec3f1eb67519b56057815393d0097
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-19 15:19:54 -0300

    Github Action

--- actions/export_env/export.sh
+++ actions/export_env/export.sh
@@ -16,8 +16,8 @@ TRES_PATH="${HOME}/.config/godot/editor_settings-3.tres"
 LINK_GODOT="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64.zip"
 LINK_TEMPLATES="https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz"
 
-sudo mkdir -p -v /root/.local/share/godot/ .config .cache
-sudo mkdir -p -v /root/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE}
+sudo mkdir -p -v ${HOME}/.local/share/godot/ .config .cache
+sudo mkdir -p -v ${HOME}/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE}
 
 echo -e "✔ Setup Godot Editor And Export Templates." 
 
@@ -29,7 +29,7 @@ sudo mv Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_linux_headless.64 ${GODOT_PATH}
 # Templates
 wget -q ${LINK_TEMPLATES}
 unzip -qq Godot_v${GODOT_VERSION}-${GODOT_RELEASE}_export_templates.tpz 
-sudo mv templates/* /root/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE}
+sudo mv templates/* ${HOME}/.local/share/godot/templates/${GODOT_VERSION}.${GODOT_RELEASE}
 
 
 echo -e "✔ Godot Editor First Launch." 
@@ -63,6 +63,7 @@ if [[ "${EXPORT_PLATFORM}" == "Linux/X11" ]]
 then 
     EXPORT_NAME="game.x86_64"
 elif [[ "${EXPORT_PLATFORM}" == "Android" ]]
+then
     EXPORT_NAME="game.debug.apk"
 fi
 
