5a901a12ca2a264d9da350766e68d26e43854b4c
Vinicius Guerrero <vini.ts.guerrero@gmail.com>
2021-07-29 13:59:09 -0300

    Github Action

--- actions/build/modules.sh
+++ actions/build/modules.sh
@@ -11,7 +11,7 @@ fi
 # MODULE_INDEX=0
 for m in "${GODOT_CUSTOM_MODULES[@]}"; do
     # sudo wget -qc "$m" -O module_${MODULE_INDEX}.zip && sudo unzip -Z1 module_${MODULE_INDEX}.zip
-    sudo wget -qc "$m" -O temp.zip && sudo unzip -Z1 temp.zip -d engine && sudo rm temp.zip
+    sudo wget -qc "$m" -O temp.zip && sudo unzip -Z1 temp.zip && sudo rm temp.zip
     # ((MODULE_INDEX++))
     # FILE=$(eval "sudo wget -qc "$m" -O temp.zip && sudo unzip -Z1 temp.zip")
     # echo -e ${FILE}
